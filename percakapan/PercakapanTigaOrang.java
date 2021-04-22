abc21c0bb5e9cbbabdb4074e86c522152e20a6a6
Mar'atus Sholihah <80057770+Maratussholihah@users.noreply.github.com>
2021-04-22 09:57:16 +0700

    Add files via upload

---
+++ percakapan/PercakapanTigaOrang.java
@@ -0,0 +1,12 @@
+
+package percakapan;
+
+public class PercakapanTigaOrang {
+
+    String Sholihah;
+    String Nanas;
+    String Salma; 
+      
+    }
+    
+
---
+++ percakapan/PercakapanTigaOrangBeraksi.java
@@ -0,0 +1,14 @@
+
+package percakapan;
+
+public class PercakapanTigaOrangBeraksi {
+    public static void main (String[] args){
+    PercakapanTigaOrang Ngobrol = new PercakapanTigaOrang ();
+    Ngobrol.Sholihah = "Hallo Nanas Hallo Salma";
+    Ngobrol.Nanas = "Oh Haii Sholihah";
+    Ngobrol.Salma = "Haii juga Sholihah";
+System.out.println(Ngobrol.Sholihah);
+System.out.println(Ngobrol.Nanas);
+System.out.println(Ngobrol.Salma);
+}
+}
