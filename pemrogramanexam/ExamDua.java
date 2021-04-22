99eabea4fce80cff1d52b61f927d9166799beeb7
Mar'atus Sholihah <80057770+Maratussholihah@users.noreply.github.com>
2021-04-22 09:55:26 +0700

    Add files via upload

---
+++ pemrogramanexam/ExamDua.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pemrogramanexam;
+
+public class ExamDua {
+    String Name;
+    int Age;
+    
+    void printName(String Name){
+        System.out.println("Name :" + Name);
+    }
+    void printAge(int Age){
+        System.out.println("Age :" + Age);
+    }
+        
+    }
+    
+
---
+++ pemrogramanexam/ExamDuaBeraksi.java
@@ -0,0 +1,14 @@
+
+package pemrogramanexam;
+
+public class ExamDuaBeraksi {
+    public static void main(String[] args){
+        ExamDua ExamDuaBeraksi = new ExamDua();
+        
+        ExamDuaBeraksi.printName("Joe Smith");
+        ExamDuaBeraksi.printAge(24);
+        
+        ExamDuaBeraksi.printName("Mary Sharp");
+        ExamDuaBeraksi.printAge(52);
+    }
+}
